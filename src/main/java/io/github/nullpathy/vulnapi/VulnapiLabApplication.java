da06dfc9c4d222803f96ed5f244db6f5db83c48d
nullpathy <333074189+Nullpathy@users.noreply.github.com>
2026-09-24 15:23:16 -0500

    chore: initialize Spring Boot project

---
+++ src/main/java/io/github/nullpathy/vulnapi/VulnapiLabApplication.java
@@ -0,0 +1,13 @@
+package io.github.nullpathy.vulnapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnapiLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnapiLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nullpathy/vulnapi/VulnapiLabApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nullpathy.vulnapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnapiLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
